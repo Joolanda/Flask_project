34ba257422cbd9b03f5e3b907e9fe0d9d8cb3d23
Joolanda <50619917+Joolanda@users.noreply.github.com>
2021-06-13 23:19:08 +0200

    basic files

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['csv', 'json'])
+
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
